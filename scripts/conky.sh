3ec2d1988feca8ba8da0283cfa41e14dcb46fc3b
Andrew Briscoe <admin@pandasportal.net>
2020-03-14 07:20:42 +0800

    Finished updating conky scripts to new version

--- scripts/conky.sh
+++ scripts/conky.sh
@@ -6,7 +6,7 @@ if pidof conky | grep [0-9] > /dev/null
 then
  exec killall conky
 else
- exec conky -c ~/.conkyrc1 & exec conky -c ~/.conkyrc2 & exec 
-conky -c ~/.conkyrc3
+ exec conky -c ~/conky/conkyrc1 & exec conky -c ~/conky/conkyrc2 & exec 
+conky -c ~/conky/conkyrc3
 
 fi
