88e1b0766c5f53b6440975873c23af08d843afea
Andrew Briscoe <admin@pandasportal.net>
2020-04-02 14:27:45 +0800

    Added RAM graphs to conky

--- conky/conky.sh
+++ conky/conky.sh
@@ -6,7 +6,5 @@ if pidof conky | grep [0-9] > /dev/null
 then
  exec killall conky
 else
- exec conky -c ~/conky/conkyrc1 & exec conky -c ~/conky/conkyrc2 & exec 
-conky -c ~/conky/conkyrc3
-
+ exec conky -c ~/conky/conkyrc1 & exec conky -c ~/conky/conkyrc2 & exec conky -c ~/conky/conkyrc3 & exec conky -c ~/conky/conkyrc4
 fi
