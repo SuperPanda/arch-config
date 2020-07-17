963ebfd71354b3c318c88bc27499247630f166e6
Andrew Briscoe <admin@pandasportal.net>
2020-07-17 21:17:56 +0800

    Updated

---
+++ vnc-user.sh
@@ -0,0 +1,1 @@
+vncserver -localhost -depth 32 -geometry 1920x1080 +extension Composite -alwaysshared :1
--- xstartup.sh
+++ xstartup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-picom &
+picom -b;
+sleep 1;
 feh --bg-fill ~/Pictures/Wallpapers/squares.png &
-killall conky
-sh ~/conky/conky.sh
-barriers --enable-crypto
+if [ $DISPLAY = ":0" ];
+then
+	killall conky;
+	sh ~/conky/conky.sh;
+	barriers --enable-crypto;
+fi
 nextcloud &
